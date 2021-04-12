726d8764cfd4ef8e5341cc6b65e01d4c6d358558
Stanislao Vargas <>
2021-04-12 14:59:40 +0000

    corrigiendo errores de compilación

--- Estudiante.cs
+++ Estudiante.cs
@@ -1,29 +1,31 @@
+using System;
+
 class Estudiante
 {
 	private string nombre;
 	private string paralelo;
-	private int edad;
+	private int gestion;
 	private double nota;
 
-	public Estudiante(string pNombre, string pParalelo, int pEdad, double pNota)
+	public Estudiante(string pNombre, string pParalelo, int pGestion, double pNota)
 	{
 		nombre = pNombre;
 		paralelo = pParalelo;
-		edad = pEdad;
+		gestion = pGestion;
 		nota = pNota;
 	}
 
-	public string Nombre {get => nombre; set => nombre = value};
-	public string Parelelo {get => paralelo; set => paralelo = value}
+	//public string Nombre {get => nombre; set => nombre = value};
+	//public string Parelelo {get => paralelo; set => paralelo = value};
 
 	public override string ToString()
 	{
-		return string.Format("{0},{1},{2},{3}", nombre, paralelo, edad, nota);
+		return string.Format("{0},{1},{2},{3}", nombre, paralelo, gestion, nota);
 	}
 
 	public double CalcularPuntosExtra()
 	{
-		return paralelo * 0.5;
+		return nota * 0.5;
 	}
 
 	public void AsignarNota()
--- main.cs
+++ main.cs
@@ -2,6 +2,9 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+    Estudiante estudiante = new Estudiante("Pedro", "Arqui1", 2020, 10);
+
+	Console.WriteLine(estudiante);
+	estudiante.CalcularPuntosExtra();
   }
 }
